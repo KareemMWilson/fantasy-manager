d7e0a86f096a397176b757eb2166789b659dbfc5
KareemWilson <kareemmagdywilson@gmail.com>
2025-01-17 22:18:20 +0200

    [feat] | players on map

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,9 +1,31 @@
+import { HStack, VStack } from "@chakra-ui/react";
+import { IoShirt } from "react-icons/io5";
+
 const Home = () => {
   return (
-    <div className="p-4">
-      <h1 className="text-2xl font-bold">Welcome to Home Page</h1>
-    </div>
+    // <Spinner width='20rem' height='20rem' color='primary.900' borderWidth='6px' opacity='0.6'/>
+    <HStack height="40rem" width="40vw">
+      <VStack id='GOALKEEPER' height="25rem" alignItems='center' justifyContent='center'>
+        <VStack style={{ fontSize: "2rem" }}><IoShirt size={60}/></VStack>
+      </VStack>
+      <VStack id='DEFENDER' height="25rem" width='10rem' alignItems='center' justifyContent='center' gap='4.5rem'>
+        <VStack style={{ fontSize: "2rem" }} alignSelf='end'><IoShirt size={60}/></VStack>
+        <VStack style={{ fontSize: "2rem" }}><IoShirt size={60}/></VStack>
+        <VStack style={{ fontSize: "2rem" }}><IoShirt size={60}/></VStack>
+        <VStack style={{ fontSize: "2rem" }}alignSelf='end'><IoShirt size={60}/></VStack>
+      </VStack>
+      <VStack id='MIDEFENDER' height="25rem" width='10rem' alignItems='center' justifyContent='center' gap='4.5rem'>
+        <VStack style={{ fontSize: "2rem" }}><IoShirt size={60}/></VStack>
+        <VStack style={{ fontSize: "2rem" }} alignSelf='end'><IoShirt size={60}/></VStack>
+        <VStack style={{ fontSize: "2rem" }}><IoShirt size={60}/></VStack>
+      </VStack>
+      <VStack id='ATTACKERS' height="25rem" width='10rem' alignItems='center' justifyContent='center' gap='4.5rem'>
+        <VStack style={{ fontSize: "2rem" }}><IoShirt size={60}/></VStack>
+        <VStack style={{ fontSize: "2rem" }}alignSelf='end'><IoShirt size={60}/></VStack>
+        <VStack style={{ fontSize: "2rem" }}><IoShirt size={60}/></VStack>
+      </VStack>
+    </HStack>
   );
 };
 
-export default Home; 
+export default Home;
