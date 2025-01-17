e6007cd4382f4e39a0e0a73fef51d9e8c301746c
KareemWilson <kareemmagdywilson@gmail.com>
2025-01-17 17:39:40 +0200

    [chore] | handle unhandledRejection and test db

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,8 +1,33 @@
+import { PrismaClient } from '@prisma/client';
 import app from './app';
 import { config } from './config';
 
 const PORT = config.PORT
 
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
   console.log(`Server running on PORT: ${PORT} `);
 });
+
+// Database connection verification
+const prisma = new PrismaClient();
+
+const connectDB = async () => {
+  try {
+    await prisma.$connect();
+    console.log('✅ Successfully connected to database');
+  } catch (error) {
+    console.error('❌ Error connecting to database:', error);
+    process.exit(1);
+  }
+};
+
+connectDB();
+
+
+process.on('unhandledRejection', (err: any) => {
+  console.error('❌ UNHANDLED REJECTION! Shutting down...');
+  console.error(err.name, err.message);
+  server.close(() => {
+    process.exit(1);
+  });
+}); 
