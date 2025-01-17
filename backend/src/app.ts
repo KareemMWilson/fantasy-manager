9f6ad9b1e200c7f11aa7a884a3dbe237f5de9f70
KareemWilson <kareemmagdywilson@gmail.com>
2025-01-17 19:46:21 +0200

    [adjust] | app providers

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,9 +1,16 @@
 import express, { Application } from 'express';
+import cors from 'cors';
 import authRouter from './routes/auth.routes'
 import { loggingMiddleware } from './middlewares/logging';
 
 const app: Application = express();
 
+app.use(cors({
+  origin: 'http://localhost:5173',
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 
 // Middleware
 app.use(express.json());
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,18 @@
 import "./App.css";
 import Navigation from "@/pages/Navigation";
-import { Provider } from 'react-redux';
-import { PersistGate } from 'redux-persist/integration/react';
-import { store, persistor } from './store';
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+import { store, persistor } from "./store";
+import { ChakraProvider } from "@chakra-ui/react";
+import system from "./theme/theme";
 
 function App() {
   return (
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <Navigation />
+        <ChakraProvider value={system}>
+          <Navigation />
+        </ChakraProvider>
       </PersistGate>
     </Provider>
   );
